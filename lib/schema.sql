b8532b96500db15102a56745c14131203d356705
Sophie Gu <sophieqgu@gmail.com>
2019-06-12 19:22:09 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -22,3 +22,12 @@ CREATE TABLE books (
   series_id INTEGER
   );
   
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY, 
+  name TEXT, 
+  motto TEXT, 
+  species TEXT, 
+  author_id INTEGER, 
+  series_id INTEGER
+  );
+  
