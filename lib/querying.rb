01e8013ee1bc12d95f7a4262355b6cc1d73d63b4
Sophie Gu <sophieqgu@gmail.com>
2019-06-12 22:39:13 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,13 +20,14 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name from authors
-   JOIN series on authors.id = series.author_id
-   JOIN subgenres on subgenres.id = series.subgenre_id"
+  "SELECT authors.name, subgenres.name FROM authors
+   JOIN series ON authors.id = series.author_id
+   JOIN subgenres ON subgenres.id = series.subgenre_id"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series 
+   "
 end
 
 def select_character_names_and_number_of_books_they_are_in
