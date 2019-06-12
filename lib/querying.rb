e9f7386c93705a03885d5937c906b451457f84d2
Sophie Gu <sophieqgu@gmail.com>
2019-06-12 22:39:55 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,7 +27,7 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series 
-   "
+   JOIN characters ON series."
 end
 
 def select_character_names_and_number_of_books_they_are_in
