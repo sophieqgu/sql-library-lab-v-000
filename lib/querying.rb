ab2c0672cf7277cafecad1ba8a1ae85408f87470
Sophie Gu <sophieqgu@gmail.com>
2019-06-12 22:51:06 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,7 +11,6 @@ def select_name_and_motto_of_char_with_longest_motto
    LIMIT 1"
 end
 
-
 def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(species) FROM characters 
    GROUP BY species
